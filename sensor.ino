569464ad433b3ddbf4f0845baaa47a6a35f42055
Yutaro HIGASHI <suica.versa@gmail.com>
2019-12-25 22:15:43 +0900

    鍵ファイルをアップして保存するエンドポイントを追加

--- sensor.ino
+++ sensor.ino
@@ -1,39 +1,121 @@
+#include <ArduinoJson.h>
+#include <PubSubClient.h>
 #include <DHT.h>
 #include <IoAbstraction.h>
 #include <WiFiClientSecure.h>
 #include <ESP8266WebServer.h>
+#include "FS.h"
 #define WIFI_RESET_PIN 14
 #define LED_PIN 4
 #define DHTPIN 13
 #define DHTTYPE DHT11
 #define MAX_WIFI_RETRY_SECONDS 10
 #define MODULE_NAME "Thermae"
+#define ROOT_CA_CERT_PATH "/rootca.pem"
+#define CERTIFICATE_PATH "/cert.pem.cert"
+#define PRIVATE_KEY_PATH "/cert.pem.key"
 
 ESP8266WebServer server(80);
 DHT dht( DHTPIN, DHTTYPE );
 
 void handleRootGet() {
-  char message[256];
-  const char* dhtText = readDht().c_str();
-  sprintf(message, "{\"message\": \"Hello from %s\", \"sensor\": \"%s\"}\n", MODULE_NAME, dhtText);
+  const int capacity = JSON_OBJECT_SIZE(3);
+  StaticJsonDocument<capacity> doc;
+  doc["message"] = String("Hello from ") + String(MODULE_NAME);
+  doc["sensor"] = readDht();
   Serial.println("Accessed / ;");
-  // String message = "{ \"message\": \"Hello from ";
-  // message.concat(MODULE_NAME);
-  // message.concat("\", ");
-  // message.concat("\"sensor\": \"");
-  // message.concat(readDht());
-  // message.concat("\"}");
 
-  server.send(200, "application/json", String(message));
+  char output[128];
+  serializeJson(doc, output);
+  server.send(200, "application/json", output);
 }
 
-void handleRoot() {
-  handleRootGet();
+void handleSettingPost() {
+  StaticJsonDocument<200> doc;
+  DeserializationError error = deserializeJson(doc, server.arg("plain"));
+  if (error) {
+    Serial.print(F("deserializeJson() failed: "));
+    Serial.println(error.c_str());
+    server.send(403);
+    return;
+  }
+
+  String cert = doc["cert"];
+  if (cert.length() > 0) {
+    Serial.println(cert);
+    File certf = SPIFFS.open(CERTIFICATE_PATH, "w");
+    if (certf) {
+      certf.print(cert);
+      certf.close();
+    }
+  }
+
+  String privateKey = doc["privateKey"];
+  if (privateKey.length() > 0) {
+    Serial.println(privateKey);
+    File privatef = SPIFFS.open(PRIVATE_KEY_PATH, "w");
+    if (privatef) {
+      privatef.print(privateKey);
+      privatef.close();
+    }
+  }
+
+  String rootCA = doc["rootCA"];
+  if (rootCA.length() > 0) {
+    Serial.println(rootCA);
+    File rootcaf = SPIFFS.open(ROOT_CA_CERT_PATH, "w");
+    if (rootcaf) {
+      rootcaf.print(rootCA);
+      rootcaf.close();
+    }
+  }  
+
+  server.send(200, "application/json", "{ \"status\": \"ok\"}");
+}
+
+void handleSettingGet() {
+  const int capacity = JSON_OBJECT_SIZE(3);
+  StaticJsonDocument<capacity> doc;
+  File f = SPIFFS.open(CERTIFICATE_PATH, "r");
+  if (f) {
+    String cert = f.readString();
+    doc["cert"] = cert;
+    Serial.print("read from fs: ");
+    Serial.println(cert);
+    f.close();
+  }
+
+  File privateF = SPIFFS.open(PRIVATE_KEY_PATH, "r");
+  if (privateF) {
+    String privateKey = privateF.readString();
+    doc["privateKey"] = privateKey;
+    Serial.print("read from fs: ");
+    Serial.println(privateKey);
+    privateF.close();
+  }
+
+  File rootCAF = SPIFFS.open(ROOT_CA_CERT_PATH, "r");
+  if (rootCAF) {
+    String rootCA = rootCAF.readString();
+    doc["rootCa"] = rootCA;
+    Serial.print("read from fs: ");
+    Serial.println(rootCA);
+    rootCAF.close();
+  }  
+  char output[128];
+  serializeJson(doc, output);
+  server.send(200, "application/json", output);
+}
+
+void prepareRouter() {
+  server.on("/", HTTP_GET ,handleRootGet);
+  server.on("/settings", HTTP_POST, handleSettingPost);
+  server.on("/cert", HTTP_GET, handleSettingGet);
 }
 
 void initializeService() {
   Serial.println("Prepareing httpd...");
-  server.on("/", handleRoot);
+  prepareRouter();
   server.begin();
   Serial.println("Server begins.");
   taskManager.scheduleFixedRate(1, []{
@@ -125,6 +207,8 @@ void initializeDevise() {
   Serial.begin(115200);
   Serial.println("Starting Module...");
   dht.begin();
+  Serial.println("Starting SPIFS....");
+  SPIFFS.begin();
   delay(100);
 }
 
